91b6105488e833004a3b4c82348847771072826f
Kamysheva1723 <natalia.kamysheva@metropolia.fi>
2023-02-25 15:26:09 +0300

    getting user_name

--- Script.py
+++ Script.py
@@ -6,17 +6,33 @@ def print_text(message, x, y, font_color=(0,0,0), font_type="C:/Users/natak/Lent
     font_type = pygame.font.Font(font_type, font_size)
     text = font_type.render(message, True, font_color)
     screen.blit(text, (x,y))
-
+    pygame.display.flip()
+def get_text():
+    need_input = True
+    input_text = ''
+    while need_input:
+        for event in pygame.event.get():
+            if need_input and event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    need_input = False
+                elif event.key == pygame.K_BACKSPACE:
+                    input_text = input_text - input_text[-1]
+                else:
+                    input_text = input_text + event.unicode
+                    print_text(input_text, 10, 300)
+    return input_text
 
 def start_game():
     screen.fill(white)
     pygame.display.flip()
     time.sleep(2)
     print_text("Aloitetaan...", 0,0)
-    pygame.display.flip()
     print_text("Anna sinun nimisi pelissamme...", 4, 30)
-    pygame.display.flip()
-    time.sleep(7)
+    user_name = get_text()
+    print_text("Hei, " + user_name, 10, 400)
+    time.sleep(20)
+
+
 
 
 pygame.init()
@@ -25,6 +41,8 @@ size = width, height = 600, 600
 speed = [1, 1]
 black = 0, 0, 0
 white = 255, 255, 255
+
+need_input = False
 
 screen = pygame.display.set_mode(size)
 
@@ -44,7 +62,6 @@ while True:
                         start_game()
 
         logo_rect = logo_rect.move([1,-1])
-
         screen.fill(white)
         screen.blit(tarina, tarina_rect)
         screen.blit(logo, logo_rect)
