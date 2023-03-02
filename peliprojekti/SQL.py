196f8fa39dd9cad6b3ce1ddf159355678ec4f44b
Kamysheva1723 <natalia.kamysheva@metropolia.fi>
2023-03-02 15:13:43 +0300

    temp

--- peliprojekti/SQL.py
+++ peliprojekti/SQL.py
@@ -3,6 +3,8 @@ DROP TABLE goal_reached;
 DROP TABLE goal;
 DROP TABLE game;
 
+Tämä taulukko tallentaa tiedot keskeneräisistä peleistä.
+
  CREATE TABLE GAME (
  player_id INTEGER not NULL PRIMARY KEY AUTO_INCREMENT, 
  time_sec INTEGER DEFAULT 0,
@@ -13,4 +15,13 @@ DROP TABLE game;
  EU_ BOOLEAN DEFAULT FALSE, NA_ BOOLEAN DEFAULT FALSE, OC_ BOOLEAN DEFAULT FALSE, 
  SA_ BOOLEAN DEFAULT FALSE,
  FOREIGN KEY (last_location) REFERENCES airport(ident) ON UPDATE CASCADE ON DELETE NO ACTION);
+ 
+
+Tämä taulukko tallentaa tiedot pelatuista peleistä eli pelaajien tuloksista.
+ 
+CREATE TABLE RESULTS (
+ player_id INTEGER NOT NULL,
+ screen_name VARCHAR(20),
+ score INTEGER,
+ time_sec INTEGER);
  """
