412a9c3e82fe0080323dd8a7fa9f6fe50642ae3f
maisajulianna <122282086+maisajulianna@users.noreply.github.com>
2023-03-09 11:18:56 +0200

    muutoksia

--- peliprojekti/sql.py
+++ peliprojekti/sql.py
@@ -5,6 +5,15 @@ DROP TABLE goal;
 DROP TABLE game;
 Tämä taulukko tallentaa tiedot keskeneräisistä peleistä.
 
+DELETE FROM airport WHERE type = 'closed' OR type = 'seaplane_base' OR TYPE = 'heliport'
+
+DELETE FROM country WHERE iso_country NOT IN (SELECT iso_country FROM airport WHERE airport.iso_country = country.iso_country)
+
+UPDATE airport
+SET municipality = 'Not in any municipality'
+WHERE municipality = ''
+
+
 CREATE TABLE GAME (
 player_id INTEGER not NULL PRIMARY KEY AUTO_INCREMENT,
 time_sec INTEGER DEFAULT 0,
@@ -18,11 +27,14 @@ FOREIGN KEY (last_location) REFERENCES airport(ident) ON UPDATE CASCADE ON DELET
 
 Tämä taulukko tallentaa tiedot pelatuista peleistä eli pelaajien tuloksista.
 
+
 CREATE TABLE RESULTS (
 player_id INTEGER NOT NULL,
 screen_name VARCHAR(20),
 score INTEGER,
 time_sec INTEGER);
+
+
 
 CREATE TABLE plane_info(
 id INT NOT NULL,
@@ -32,45 +44,10 @@ questions INT(3) null,
 velocity INT(3)null,
 type VARCHAR(40));
 INSERT INTO plane_info(id, emission, risk, questions, velocity, type)
-VALUES (1, 2, 2, 16, 8,'vähänpästoinen');
+VALUES (1, 2, 20, 4, 8,'vähänpästoinen');
 INSERT INTO plane_info(id, emission, risk, questions, velocity, type)
-VALUES (2, 4, 2, 12, 8,'matkustajankone');
+VALUES (2, 2, 20, 4, 8,'matkustajankone');
 INSERT INTO plane_info(id, emission, risk, questions, velocity, type)
-VALUES (3, 8, 4, 8, 4,'yksityiskone');
+VALUES (3, 8, 40, 2, 4,'yksityiskone');
 INSERT INTO plane_info(id, emission, risk, questions, velocity, type)
-VALUES (4, 16, 8, 4, 2,'hävittäjä');
-
-
-
-DROP TABLE IF EXISTS
-
-
-
-CREATE TABLE start_airports(
-  id int(11) NOT null,
-  ident varchar(40) NOT NULL,
-  type varchar(40) DEFAULT NULL,
-  name varchar(40) DEFAULT NULL,
-  continent varchar(40) DEFAULT NULL,
-  iso_country varchar(40) DEFAULT NULL,
-  municipality varchar(40) DEFAULT NULL,
-  keywords varchar(40) DEFAULT NULL,
-  PRIMARY KEY (ident),
-  FOREIGN KEY (iso_country) REFERENCES country(iso_country)
-  )start_airports
-
-INSERT INTO plane_info(id, emission, risk, questions, velocity)
-VALUES ('1', '2', '20', '4', '8'), ('2', '2', '20', '4', '8'), ('3', '8', '40', '2', '4'), ('4', '16', '60', '1', '2');
-
-
-SELECT * from airport where ident = 'DNMM' OR ident = 'ZBAA' OR ident = 'EDDF' OR ident = 'KLAS' OR ident = 'YSSY' OR ident = 'SBJH'
-
-
-DELETE FROM airport WHERE type = 'closed' OR type = 'seaplane_base' OR TYPE = 'heliport'
-
-DELETE FROM country WHERE iso_country NOT IN (SELECT iso_country FROM airport WHERE airport.iso_country = country.iso_country)
-
-UPDATE airport
-SET municipality = 'Not in any municipality'
-WHERE municipality = ''
-"""
+VALUES (4, 16, 60, 1, 2,'hävittäjä');
